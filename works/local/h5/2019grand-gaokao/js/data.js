c15f83e8e922a9bc05e172a96cd16115dbd58f0c
Wubin <634174214@qq.com>
2026-08-18 16:47:04 +0800

    2019高考

---
+++ works/local/h5/2019grand-gaokao/js/data.js
@@ -0,0 +1,6 @@
+"use strict";
+var bannerBase64 = 'imgs/data-banner.jpg',
+  boxBase64 = 'imgs/data-box.png',
+  toptextBase64 = 'imgs/data-toptext.png',
+  bottomBase64 = 'imgs/data-bottom.png',
+   toplogoBase64 = 'imgs/data-toplogo.png';
